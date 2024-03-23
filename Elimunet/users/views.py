b6ef0bac8e1afc1040a66dfa345c96d42d492aa5
codeschris <chrismlolwa42@outlook.com>
2024-03-23 11:37:39 +0300

    Modify redirect links for roles - The redirects depending on the roles have been modified to work according to role selected when registering - Pending instructor dashboard page

--- Elimunet/users/views.py
+++ Elimunet/users/views.py
@@ -7,8 +7,16 @@ from .forms import UserRegisterForm
 def homepage(request):
     return render(request, 'homepage.html')
 
+# Modify these views depending on what you want it to achieve
 def student_dashboard(request):
     return render(request, 'users/student_dashboard.html')
+
+def instructor_dashboard(request):
+    return render(request, 'users/instructor_dashboard.html')
+
+def admin_dashboard(request):
+    return render(request, 'users/admin_dashboard.html')
+# Modify the above views depenging on what you want to achieve
 
 def register(request):
     if request.method == 'POST':
@@ -38,16 +46,16 @@ def role_based_redirect(request):
 def student_dashboard(request):
     if request.user.role != CustomUser.STUDENT:
         return redirect('role_based_redirect')
-    return render(request, 'dashboard/student_dashboard.html')
+    return render(request, 'users/student_dashboard.html')
 
 @login_required
 def instructor_dashboard(request):
     if request.user.role != CustomUser.INSTRUCTOR:
         return redirect('role_based_redirect')
-    return render(request, 'dashboard/instructor_dashboard.html')
+    return render(request, 'users/instructor_dashboard.html')   # Create a page for the instructor admin
 
 @login_required
 def admin_dashboard(request):
     if request.user.role != CustomUser.ADMIN:
         return redirect('role_based_redirect')
-    return render(request, 'dashboard/admin_dashboard.html')
+    return render(request, 'users/admin_dashboard.html')
